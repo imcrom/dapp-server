ef21244e3e5efd889e868da0c7699409aa78658d
crom <kbofc@protonmail.com>
2023-03-10 23:04:31 +0800

    initial commit 2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 var cors = require('cors');
-const { readFile } = require('fs').promises;
+const { readFile, writeFile } = require('fs').promises;
 const app = express();
 
 var corsOptions = {
@@ -28,13 +28,52 @@ app.get("/api",cors(corsOptions), (req, res) => {
     res.json({"message": "Hello from server!"});
 });
 
+
+async function writeToJson(user) {
+  try {
+    const users = await readFile('./users.json', 'utf8');
+    const userList = JSON.parse(users);
+    userList.push(user);
+    await writeFile('./users.json', JSON.stringify(userList, null, 2));
+    console.log(`User ${user.username} added to users.json`);
+  } catch (err) {
+    console.error('Error occurred while writing to file!', err);
+  }
+}
+
+
+async function checkUser(username) {
+  try {
+    const users = await readFile('./users.json', 'utf8');
+    const userList = JSON.parse(users);
+    const userExists = userList.some(user => user.username === username);
+    return userExists;
+  } catch (err) {
+    console.error('Error occurred while reading directory!', err);
+    return false;
+  }
+}
+
+
 app.post("/api", cors(corsOptions), async (req, res) => {
 
-  const data = req.body
+  const data = req.body;
+  const username = data.username;
+  const wallAddress = data.wall_address;
+
 
   async function listDir() {
     try {
       const users = await readFile('./users.json', 'utf8');
+
+      const userExists = await checkUser(username);
+      if (userExists) {
+        res.status(201).json({ message: 'User exists' });
+      } else {
+        const user = { username, wall_address: wallAddress };
+        await writeToJson(user);
+        res.status(200).json({ message: 'User added to list' });
+      }
       console.log(JSON.stringify(users));
     } catch (err) {
       console.error('Error occurred while reading directory!', err);
