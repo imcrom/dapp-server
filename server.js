26c28b418f10d5420dca33d1d2b92d959dc7f49e
imcrom <kbofc@protonmail.com>
2023-03-29 11:22:02 +0800

    added dan

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const request = require('request');
 var cors = require('cors');
 const { readFile, writeFile } = require('fs').promises;
 const app = express();
+const app2 = express();
 
 var corsOptions = {
   origin: 'https://gatekeep.magic-ai.org',
@@ -19,8 +20,6 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
-
-
 
 
 
@@ -91,7 +90,87 @@ app.post("/api", cors(corsOptions), async (req, res) => {
 
     
 });
+
+
+var corsOptions2 = {
+  origin: 'https://dapp.dan-ai.io',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+
+app2.use(express.json());
+app2.use(cors({ origin: 'https://dapp.dan-ai.io' }));
+app2.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://dapp.dan-ai.io');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+
+app2.get("/get_users2", cors(corsOptions2), async (req, res) => {
+  try {
+    const data = await readFile("./dan-users.json", "utf8");
+    res.json(JSON.parse(data));
+  } catch (err) {
+    console.error("Error occurred while reading users file!", err);
+    res.status(500).json({ error: "Unable to read users file" });
+  }
+});
+
+async function writeToJson2(user) {
+  try {
+    const users = await readFile('./dan-users.json', 'utf8');
+    const userList = JSON.parse(users);
+    userList.push(user);
+    await writeFile('./dan-users.json', JSON.stringify(userList, null, 2));
+    console.log(`User ${user.username} added to users.json`);
+  } catch (err) {
+    console.error('Error occurred while writing to file!', err);
+  }
+}
+
+
+async function checkUser2(username) {
+  try {
+    const users = await readFile('./dan-users.json', 'utf8');
+    const userList = JSON.parse(users);
+    const userExists = userList.some(user => user.username === username);
+    return userExists;
+  } catch (err) {
+    console.error('Error occurred while reading directory!', err);
+    return false;
+  }
+}
   
+app2.post("/apiv2", cors(corsOptions2), async (req, res) => {
+
+  const data = req.body;
+  const username = data.username;
+  const wallAddress = data.wall_address;
+
+
+  async function listDir() {
+    try {
+      const users = await readFile('./dan-ai.json', 'utf8');
+
+      const userExists = await checkUser2(username);
+      if (userExists) {
+        res.status(201).json({ message: 'User exists' });
+      } else {
+        const user = { name: username, wall_address: wallAddress };
+        await writeToJson2(user);
+        res.status(200).json({ message: 'User added to list' });
+      }
+      console.log(JSON.stringify(users));
+    } catch (err) {
+      console.error('Error occurred while reading directory!', err);
+    }
+  }
   
+  await listDir();
+
+    
+});
 
 app.listen(5000, () => {console.log("server started on port 80")});
